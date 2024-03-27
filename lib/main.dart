4f68e869dbb0c4a10f43e18a217d01a1af070123
KimSaHwan <ksh-3013@naver.com>
2024-03-27 19:50:36 +0900

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,82 +10,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-<<<<<<<<< Temporary merge branch 1
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.greenAccent),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'VIDA'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
-        titleTextStyle: const TextStyle(color: Colors.white, fontSize: 30, fontFamily: "bhs"),
-      ),
-      body: const Center(
-        child: Text("이 어플리케이션은 여러분의 건강을 책임집니다.")
-      ),
-      //신체 검사 (AI적용)
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.sensor_occupied),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.white,
-        items: const [
-          //기본화면
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "홈"),
-          //커뮤니티나 팁 겁색 화면
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: "검색"),
-          //마이 어카운트
-          BottomNavigationBarItem(icon: Icon(Icons.account_circle), label: "프로필"),
-        ],
-      ),
-=========
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color(0xFFF0997D),
-          title: const Text(
-            "무럭무럭",
-            style: TextStyle(
-              color: Colors.white,
-              fontFamily: "sbb", // 폰트 이름만 전달
-              fontSize: 30,
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: const Color(0xFFF0997D),
+            title: const Text(
+              "무럭무럭",
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: "sbb",
+              ),
             ),
           ),
-        ),
-        body: Container(
-        ),
-      ),
->>>>>>>>> Temporary merge branch 2
+        )
     );
   }
 }
